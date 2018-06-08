8e6b88a2ad3632e631e9a33e1ffe32e934f718c1
futu11 <pedrofelipe.amorim@hotmail.com>
2018-06-08 12:55:00 -0300

    Add files via upload

---
+++ comprecao.h
@@ -0,0 +1,36 @@
+#ifndef COMPRECAO_H_INCLUDED
+#define COMPRECAO_H_INCLUDED
+
+typedef struct no{
+    char elemento;
+    int repeticao;
+    struct no *esq;
+    struct no *dir;
+}tNo;
+
+typedef struct listano{
+    char elemento;
+    struct listano *prox;
+} tListaNo;
+
+typedef struct repet{
+    char elemento;
+    int repeticao;
+    struct repet *proxalfa;
+}tRepeticao;
+
+
+extern tListaNo * getListaNo(char elemento);
+extern tRepeticao *getRepeticao(char elemento, int rep);
+extern tNo * getNo(char elemento, int repeticao);
+
+extern void IniciaRepeticao(tRepeticao **repeticao);
+extern void IniciaLista(tListaNo **lista);
+extern int InsereElementoEmOrdem(tListaNo **lista, char elemento);
+extern void IniciaArvore(tNo **arv);
+extern void CriaArvore(tNo**arv, tRepeticao **repeticao);
+extern int ListaRepeticoes(tListaNo **lista, tRepeticao **repeticao);
+extern void Armazena(tNo **arv, tListaNo *lista);
+extern int InsereElementoEmOrdemRepeticao(tRepeticao **repeticao, char elemento , int repet);
+
+#endif // COMPRECAO_H_INCLUDED
